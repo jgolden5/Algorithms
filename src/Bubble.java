b956b39f7815494197a88ca553463dd85468d1f2
jgolden5 <jonathan.golden.gurr@gmail.com>
2025-01-24 09:14:05 -0700

    bubble sort

--- src/Bubble.java
+++ src/Bubble.java
@@ -1,5 +1,15 @@
 public class Bubble {
   public static int[] sort(int[] start) {
-    return start;
+    int[] result = start;
+    for(int i = 0; i < start.length; i++) {
+      for(int j = 0; j < start.length - i - 1; j++) {
+        if(result[j] > result[j + 1]) {
+          int temp = result[j];
+          result[j] = result[j + 1];
+          result[j + 1] = temp;
+        }
+      }
+    }
+    return result;
   }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
+import java.util.Arrays;
+
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello Algs");
+    int[] unsorted1 = new int[]{3, 9, 4, 6, 8, 1, 0, 2, 7, 5};
+    System.out.println("Unsorted = " + Arrays.toString(unsorted1));
+    int[] sorted1 = Bubble.sort(unsorted1);
+    System.out.println("Sorted   = " + Arrays.toString(sorted1));
+
+    int[] unsorted2 = new int[]{9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
+    System.out.println("Unsorted = " + Arrays.toString(unsorted2));
+    int[] sorted2 = Bubble.sort(unsorted2);
+    System.out.println("Sorted   = " + Arrays.toString(sorted2));
+
+    int[] unsorted3 = new int[]{0, 6, 3, 3, 3, 3, 3, 3, 9, 4};
+    System.out.println("Unsorted = " + Arrays.toString(unsorted3));
+    int[] sorted3 = Bubble.sort(unsorted3);
+    System.out.println("Sorted   = " + Arrays.toString(sorted3));
   }
 }
